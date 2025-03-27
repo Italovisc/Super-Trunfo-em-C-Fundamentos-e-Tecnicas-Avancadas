999f3ce34fa29c1fcf8af81914a297b55e2a20f7
Italo Santos <italotsantana75@gmail.com>
2025-03-27 11:00:49 -0300

    Organização de pastas prática de outra aula

---
+++ 1_Nivel_Novato/Aula_4/variaveis_e_tipos_de_dados.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 25;
+    int quantidade = 1;
+    float altura = 1.80;
+    double peso = 90.5;
+    char letra = 'A';
+    char nome[20] = "Italo";
+
+    return 0;
+}
