92995b8484530d1e3580608d99c3f711e667c5e1
Italo Santos <italotsantana75@gmail.com>
2025-04-27 15:19:50 -0300

    Prática da aula 4

---
+++ 2_Nivel_Aventureiro/Aula_4/manipulacao_conversao_dados.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Criação das variáveis e atribuições
+    int a = 10, b = 3;
+
+    float quociente = (float) a / b;
+
+
+    // Outupt de dados
+    printf("Quociente: %.2f", quociente);
+
+
+    return 0;
+}
